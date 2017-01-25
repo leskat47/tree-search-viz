782af280672864b56d51cfc2b951bdfba7c11853
leslie <leslie@hackbrightacademy.com>
2017-01-25 15:35:00 -0800

    Explicitly state var when initiating

--- static/js/treesearch.js
+++ static/js/treesearch.js
@@ -43,9 +43,9 @@
     };
 
   // set the dimensions and margins of the diagram
-  var margin = {top: 40, right: 90, bottom: 50, left: 90},
-      width = 660 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+  var margin = {top: 40, right: 90, bottom: 50, left: 90};
+  var width = 660 - margin.left - margin.right;
+  var height = 500 - margin.top - margin.bottom;
 
   // declares a tree layout and assigns the size
   var treemap = d3.tree()
