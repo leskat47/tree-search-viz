39a998c2c318330fe66d2608908e703fde974279
leslie <leslie@hackbrightacademy.com>
2017-01-26 10:53:58 -0800

    Remove classes of node internal and leaf

--- static/js/treesearch.js
+++ static/js/treesearch.js
@@ -42,62 +42,63 @@
       ],
     };
 
-  // set the dimensions and margins of the diagram
-  var margin = {top: 40, right: 90, bottom: 50, left: 90};
-  var width = 660 - margin.left - margin.right;
-  var height = 500 - margin.top - margin.bottom;
+  function buildTree () {
+    // set the dimensions and margins of the diagram
+    var margin = {top: 40, right: 90, bottom: 50, left: 90};
+    var width = 660 - margin.left - margin.right;
+    var height = 500 - margin.top - margin.bottom;
 
-  // declares a tree layout and assigns the size
-  var treemap = d3.tree()
-      .size([width, height]);
+    // declares a tree layout and assigns the size
+    var treemap = d3.tree()
+        .size([width, height]);
 
-  //  assigns the data to a hierarchy using parent-child relationships
-  var nodes = d3.hierarchy(treeData);
+    //  assigns the data to a hierarchy using parent-child relationships
+    var nodes = d3.hierarchy(treeData);
 
-  // maps the node data to the tree layout
-  nodes = treemap(nodes);
+    // maps the node data to the tree layout
+    nodes = treemap(nodes);
 
-  // append the svg obgect to the body of the page
-  // appends a 'group' element to 'svg'
-  // moves the 'group' element to the top left margin
-  var svg = d3.select("#graph").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom),
-      g = svg.append("g")
-        .attr("transform",
-              "translate(" + margin.left + "," + margin.top + ")");
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select("#graph").append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom),
+        g = svg.append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
 
-  // adds the links between the nodes
-  var link = g.selectAll(".link")
-      .data( nodes.descendants().slice(1))
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("d", (d) =>
-         "M" + d.x + "," + d.y +
-         "C" + d.x + "," + (d.y + d.parent.y) / 2 +
-          " " + d.parent.x + "," +  (d.y + d.parent.y) / 2 +
-          " " + d.parent.x + "," + d.parent.y
-      );
+    // adds the links between the nodes
+    var link = g.selectAll(".link")
+        .data( nodes.descendants().slice(1))
+      .enter().append("path")
+        .attr("class", "link")
+        .attr("d", (d) =>
+           "M" + d.x + "," + d.y +
+           "C" + d.x + "," + (d.y + d.parent.y) / 2 +
+            " " + d.parent.x + "," +  (d.y + d.parent.y) / 2 +
+            " " + d.parent.x + "," + d.parent.y
+        );
 
-  // adds each node as a group
-  var node = g.selectAll(".node")
-      .data(nodes.descendants())
-    .enter().append("g")
-      .attr("class", (d) =>
-        "node" + (d.children ? " node--internal" : " node--leaf"))
-      .attr("transform", (d) => "translate(" + d.x + "," + d.y + ")")
-      .attr("id", (d) => d.data.name);
+    // adds each node as a group
+    var node = g.selectAll(".node")
+        .data(nodes.descendants())
+      .enter().append("g")
+        .attr("class", (d) => "node")
+        .attr("transform", (d) => "translate(" + d.x + "," + d.y + ")")
+        .attr("id", (d) => d.data.name);
 
-  // adds the circle to the node
-  node.append("circle")
-    .attr("r", 10)
+    // adds the circle to the node
+    node.append("circle")
+      .attr("r", 10)
 
-  // adds the text to the node
-  node.append("text")
-    .attr("dy", ".35em")
-    .attr("y", (d) => d.children ? -20 : 20) // place text above circle unless it's the bottom/last node
-    .text(d => d.data.name);
+    // adds the text to the node
+    node.append("text")
+      .attr("dy", ".35em")
+      .attr("y", (d) => d.children ? -20 : 20) // place text above circle unless it's the bottom/last node
+      .text(d => d.data.name);
 
+  }
 
   // Change circles' appearance per status attributes
   function updateCircles() {
@@ -116,7 +117,7 @@
         );
   }
 
-
+  
   // Change searching text to show status
   function updateText(queueList, current){
 
@@ -230,6 +231,9 @@
       d3.select(this).datum().done = false;
     });
   }
+
+
+  buildTree();
 
   // Event listeners
   d3.selectAll(".start-search-button").on("click", initSearch);
